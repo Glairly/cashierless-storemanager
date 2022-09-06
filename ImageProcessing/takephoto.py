99c82e6d7b0a48969285c5b3cb457868b34f2848
GookGai <kaiyang18110@gmail.com>
2022-09-07 00:07:44 +0700

    Update takephoto.py
    change filename after capturing

--- ImageProcessing/takephoto.py
+++ ImageProcessing/takephoto.py
@@ -1,11 +1,12 @@
 import cv2
 video_capture = cv2.VideoCapture(0)
 i = 0
+objectClass = "Karamujo"
 while True:
     _,frame = video_capture.read()
     cv2.imshow("test",frame)
     if cv2.waitKey(1) & 0xFF == ord('p'):
-        cv2.imwrite('pic'+str(i)+'.png', frame)
+        cv2.imwrite(objectClass+str(i)+'.png', frame)
         i += 1
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
