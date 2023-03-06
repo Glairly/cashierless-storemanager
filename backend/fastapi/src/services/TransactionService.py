7f211586890941570ad0e57fbfb33c2906f932b0
Wongvarit Pancharoen <oakbahina@gmail.com>
2023-03-07 00:13:38 +0700

    fix: fetch transaction api

--- backend/fastapi/src/services/TransactionService.py
+++ backend/fastapi/src/services/TransactionService.py
@@ -15,10 +15,10 @@ class TransactionService:
     def __init__(self, mongoClient: Collection) -> None:
         self.__mongoClient = mongoClient
 
-    def convert_to_str(self, result: any) -> str:
+    def convert_to_str(self, result: any):
         result['_id'] = str(result['_id'])
-        result['shop_id'] = str(result['client_id'])
-        result['wallet_id'] = str(result['shop_id'])
+        result['client_id'] = str(result['client_id'])
+        result['shop_id'] = str(result['shop_id'])
         # result['item_id'] = map(lambda x: str(x), result['item_id'])
         return result
 
@@ -29,23 +29,29 @@ class TransactionService:
     def get_transaction_by_client_id(self, _id: str) -> List[Transaction]:
         cursor = self.__mongoClient.find({"client_id": ObjectId(_id)})
         results = []
-        while cursor.alive:
-            result = cursor.next()
-            results.append(self.convert_to_str(result))
+        try:
+            while True:
+                result = cursor.next()
+                results.append(self.convert_to_str(result))
+        except:
+            pass
         return results
     
     def get_transaction_by_shop_id(self, _id: str) -> List[Transaction]:
         cursor = self.__mongoClient.find({"shop_id": ObjectId(_id)})
         results = []
-        while cursor.alive:
-            result = cursor.next()
-            results.append(self.convert_to_str(result))
+        try:
+            while True:
+                result = cursor.next()
+                results.append(self.convert_to_str(result))
+        except:
+            pass
         return results
     
     def create_transaction(self, clientId: str, shopId: str, items: List[TransactionItem], barcodes: List[str], total_items: int, total_price: float):
         self.__mongoClient.insert_one({
-                'client_id': clientId,
-                'shop_id': shopId,
+                'client_id': ObjectId(clientId),
+                'shop_id': ObjectId(shopId),
                 'items': [ dict(x) for x in items],
                 'barcodes': barcodes,
                 'total_items': total_items,
