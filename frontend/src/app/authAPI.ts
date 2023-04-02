3e5dbafa494aeea3cf614645099aec2ae55d4373
cherpomm <cherpomm@gmail.com>
2023-04-02 21:36:09 +0700

    refactor: register field

--- frontend/src/app/authAPI.ts
+++ frontend/src/app/authAPI.ts
@@ -5,6 +5,7 @@ import { Action } from 'redux';
 
 import { DefaultApi } from './api';
 import { LoginCapiV1LoginPostRequest } from './api';
+import { SignupCapiV1SignupPostRequest } from './api';
 
 export const login = (username: string, password: string): ThunkAction<void, RootState, null, Action<string>> => async dispatch => {
   try {
@@ -19,6 +20,28 @@ export const login = (username: string, password: string): ThunkAction<void, Roo
     const { access_token, user} = res;
     dispatch(setToken(access_token));
     dispatch(setUser(user))
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const register = (values: any): ThunkAction<void, RootState, null, Action<string>> => async dispatch => {
+  try {
+    const request = {
+      signUpRequest: {
+        username: values.username,
+        password: values.password,
+        email: values.email,
+        name: values.name,
+        isShopOwner: values.is_shop_owner,
+        gender: values.gender,
+        birthdate: values.birth_date,
+        phoneNumber: values.phone_number,
+        faceImg: values.face_img
+      }
+    } as SignupCapiV1SignupPostRequest;
+
+    const res = await (new DefaultApi()).signupCapiV1SignupPost(request)
   } catch (error) {
     console.error(error);
   }
