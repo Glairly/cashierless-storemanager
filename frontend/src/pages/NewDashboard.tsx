a1a55bd67a871542cf7c9f60ffe86d07de3fad2c
cherpomm <cherpomm@gmail.com>
2023-04-18 05:00:54 +0700

    add: new dashboard with redux

--- frontend/src/pages/NewDashboard.tsx
+++ frontend/src/pages/NewDashboard.tsx
@@ -1,5 +1,10 @@
+import { useEffect, useState } from "react";
 import { Button, Card, Image, Table } from "react-bootstrap";
 import { BsCurrencyDollar, BsFillCheckCircleFill, BsFillDashCircleFill } from "react-icons/bs";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchWallet } from "../features/auth/authAPI";
+import { fetchClientTransaction, getAllShop } from "../features/transaction/transactionAPI";
+import { RootState } from "../app/store";
 
 const mock = [
   {
@@ -31,7 +36,38 @@ const mock = [
   }
 ]
 
+interface shop {
+  id: number;
+  name: string;
+}
+
 const NewDashboard: React.FC = () => {
+  const [shop, setShop] = useState<shop[]>();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch<any>(fetchWallet());
+    dispatch<any>(fetchClientTransaction());
+    dispatch<any>(getAllShop()).then((result: any) => setShop(result));
+    console.log(shop);
+  }, [dispatch]);
+
+  const handleDateFormat = (strDate: string): string => {
+    const date = new Date(strDate);
+    const formattedDate = date.toLocaleString();
+    return formattedDate
+  }
+
+  const handleShopName = (shop_id: number): shop | undefined => {
+    const obj = shop?.find(key => key.id === shop_id)
+    return obj;
+  }
+  const wallet = useSelector((state: RootState) => state.auth.wallet);
+  const clientTransaction = useSelector(
+    (state: RootState) => state.transaction.clientTransaction
+  );
+
   return (
     <div className="p-3 bg-light">
       <div className="container-fluid">
@@ -40,7 +76,7 @@ const NewDashboard: React.FC = () => {
             <Card.Body>
               <div className="d-flex justify-content-between py-3">
                 <div className="d-flex flex-column justify-content-center">
-                  <span className="fs-2 fw-bold" style={{ color: "#ff9600" }}>$ 1550.56</span>
+                  <span className="fs-2 fw-bold" style={{ color: "#ff9600" }}>$ {wallet?.balance.toFixed(2)}</span>
                   <span style={{ color: "#ff9600" }}>Current Cashierless Wallet Ballance</span>
                 </div>
                 <div className="d-flex flex-column justify-content-center">
@@ -60,32 +96,38 @@ const NewDashboard: React.FC = () => {
               </tr>
             </thead>
             <tbody>
-              {mock.map((transaction) => (
+              {clientTransaction.slice(0, 3).map((transaction) => (
                 <tr className="align-middle" key={transaction.id}>
                   <td className="py-3 ps-2">
                     <div className="d-flex">
                       <Image
                         roundedCircle
                         style={{ width: "50px", height: "50px" }}
-                        src={transaction.shop_profile}
+                        src={"https://simplyilm.com/wp-content/uploads/2017/08/temporary-profile-placeholder-1.jpg"}
                         className="align-self-center me-2"
                       />
                       <div className="d-flex flex-column justify-content-center">
-                        <span className="fw-bold">{transaction.shop_name}</span>
+                        <span className="fw-bold">{handleShopName(transaction.shop_id)?.name || "Unkown Shop"}</span>
                         <small>Transaction ID: {transaction.id}</small>
-                        <small>{transaction.date}</small>
+                        <small>{handleDateFormat(transaction.date) || "Unknown Date"}</small>
                       </div>
                     </div>
                   </td>
-                  <td className="text-center">{transaction.items}</td>
-                  <td className="text-center">{transaction.price}</td>
+                  <td className="text-center">{transaction.transaction_items.length === 0 ? "-" :
+                    transaction.transaction_items.map((x) => (
+                      <>
+                        {x.item_id} x {x.quantity}
+                        <br />
+                      </>
+                    ))}</td>
+                  <td className="text-center">{transaction.total_price}</td>
                   <td>
                     <div
                       className="d-flex flex-column align-items-center"
-                      style={transaction.status === "Complete" ? { color: "#43db00" } : { color: "red" }}
+                      style={"Complete" === "Complete" ? { color: "#43db00" } : { color: "red" }}
                     >
-                      {transaction.status === "Complete" ? <BsFillCheckCircleFill /> : <BsFillDashCircleFill />}
-                      <small>{transaction.status}</small>
+                      {"Complete" === "Complete" ? <BsFillCheckCircleFill /> : <BsFillDashCircleFill />}
+                      <small>{"Complete"}</small>
                     </div>
                   </td>
                 </tr>
